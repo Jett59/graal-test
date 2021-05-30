1ad535b46d870328adf4e0a92c573b2c171dcc31
Jett Thompson <jett.thompson.59@gmail.com>
2021-05-30 14:34:15 +1000

    made it possible to open files

--- src/main/java/app/cleancode/graal_test/ExplorerView.java
+++ src/main/java/app/cleancode/graal_test/ExplorerView.java
@@ -1,5 +1,6 @@
 package app.cleancode.graal_test;
 
+import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -7,20 +8,13 @@ import java.util.stream.Stream;
 
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Group;
-import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Screen;
@@ -34,11 +28,12 @@ public void start (Pane root, Screen screen) {
 		path.set(System.getProperty("user.home"));
 		BorderPane wholeScreen = new BorderPane();
 		FlowPane files = new FlowPane();
-		Text pathText = new Text();
+		TextField pathText = new TextField();
+		pathText.setEditable(false);
 		pathText.textProperty().bind(path);
 		pathText.setFont(new Font(48));
 		wholeScreen.setTop(pathText);
-		wholeScreen.setAlignment(pathText, Pos.CENTER);
+		BorderPane.setAlignment(pathText, Pos.CENTER);
 		wholeScreen.setCenter(new ScrollPane(files));
 		root.setStyle("-fx-background-color: white;");
 		root.getChildren().add(wholeScreen);
@@ -51,9 +46,21 @@ public void start (Pane root, Screen screen) {
 	}
 }
 public void populateFiles (FlowPane root) throws Exception {
+	root.getChildren().clear();
 	Stream<Path> files = Files.list(Paths.get(path.get()));
 	files.map(Path::getFileName).map(Path::toString).forEach(file -> {
 		Button fileButton = new Button(file);
+		fileButton.setOnAction(evt -> {
+			if (new File(Paths.get(path.get(), file).toAbsolutePath().toString()).isDirectory()) {
+			path.set(Paths.get(path.get(), file).toAbsolutePath().toString());
+			try {
+				populateFiles(root);
+			} catch (Exception e) {
+				
+			}
+			}
+		});
+		fileButton.setPrefWidth(Screen.getPrimary().getBounds().getWidth());
 		root.getChildren().add(fileButton);
 	});
 }
